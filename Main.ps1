21ebf01ea69dfb2108a84c945ffdaa87f1556acd
victorcruz2021 <85695094+victorcruz2021@users.noreply.github.com>
2022-04-12 15:51:46 -0500

    first commit
    first commit with AD validation

---
+++ Main.ps1
@@ -0,0 +1,14 @@
+
+#Validates the Domain name Joined to proceed
+$Domain = $hostname = $null
+$Domain = (Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem).domain
+$hostname = (Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem).hostname
+
+if ( $Domain -eq "explore.local"  ){
+
+    Write-Output "The Domain is correct and can continue ${hostname}
+    Domain name = ${Domain}"
+}else{
+    Write-Output "This is not a Domain Joined server
+    Domain name = ${Domain}"
+}
